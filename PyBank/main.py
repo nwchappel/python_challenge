e59d86e8096cf29b8bc701c887dfb7c420a94947
Nicholas Chappel <nwchappel@Nicholass-MacBook-Pro.local>
2019-11-11 18:21:11 -0500

    fixed totaling issue

--- PyBank/main.py
+++ PyBank/main.py
@@ -22,5 +22,4 @@ with open(budget_csv, newline="") as csvfile:
         row_totals = row[1]
         #print(row_totals)
         total.append(int(row_totals))
-        print(f"Total: {sum(total)}")
-
+    print(f"Total: {sum(total)}")
