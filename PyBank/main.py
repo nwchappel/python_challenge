cc0bf8e32db2e17d93df98bfb760251f8ec53d5a
Nicholas Chappel <nwchappel@Nicholass-MacBook-Pro.local>
2019-11-12 13:40:35 -0500

    Done with all but creating txt file

--- PyBank/main.py
+++ PyBank/main.py
@@ -27,3 +27,25 @@ with open(budget_csv, newline="") as csvfile:
     for i in range(len(total)-1):
         average_change.append(total[i+1] - total[i])
     print(f"Average Change: ${round(sum(average_change) / len(average_change),2)}")
+
+greatest_increase_profits = max(average_change)
+greatest_decrease_profits = min(average_change)
+
+# print(greatest_increase_profits)
+# print(greatest_decrease_profits)
+
+months = []
+
+with open(budget_csv, newline="") as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+    csv_header = next(csvfile)
+    for row in csvreader:
+        month_totals = row[0]
+        # print(month_totals)
+        months.append(str(month_totals))
+
+greatest_increase_month = average_change.index(max(average_change)) + 1
+greatest_decrease_month = average_change.index(min(average_change)) + 1
+
+print(f"Greatest Increase in Profits: {months[greatest_increase_month]} (${(str(greatest_increase_profits))})")
+print(f"Greatest Decrease in Profits: {months[greatest_decrease_month]} (${(str(greatest_decrease_profits))})")
